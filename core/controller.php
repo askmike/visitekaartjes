06cb8ecb652248ace6df8b31fd65757d80020820
Mike van Rossum <mike@askmike.org>
2011-11-30 01:18:47 +0100

    quick fixed getPage method bug

--- core/controller.php
+++ core/controller.php
@@ -10,7 +10,7 @@ class Controller{
 		$this->load = new Load();
 		$this->model = new Model();
 		
-		//check which page is requested
+		//check which page is requesten
 		if(!isset($_POST['submit']) || !isset($_POST['naam']) || !isset($_POST['beroep'])) {
 			$this->home();
 		} else {
@@ -19,14 +19,18 @@ class Controller{
 	}
 	
 	function home() {
-		$this->load->view('header');
-		$this->load->view('form');
-		$this->load->view('footer');
+		$this->getPage('form', false);
 	}
 	
 	function result() {
 		$data = $this->model->getData();
 		$this->getPage('results', $data);
+	}
+	
+	function getPage($view, $data) {  	
+		$this->load->view('header');
+		$this->load->view($view, $data);
+		$this->load->view('footer', $data);
 	}
 }
 
