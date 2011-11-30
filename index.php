d0a6a5cdfc27ac846ea55efc5de34cc5a679b6b2
Mike van Rossum <mike@askmike.org>
2011-11-30 01:08:44 +0100

    removed old code

--- index.php
+++ index.php
@@ -6,36 +6,4 @@ require 'core/controller.php';
 
 new Controller();
 
-/*$load = new Load;
-
-if(!isset($_POST['submit'])) {
-	home();
-} else {
-	//we need a model to process data
-	require 'core/model.php';
-	results();
-}
-
-function home() {
-	getPage($load, 'form');
-}
-
-function results() {
-	//we need to process data so we need the model
-	require('core/model.php');
-	
-	//get all the data
-	$model = new Model();
-	extract($model->data);
-	
-	//get the results
-	getPage($load, 'results');
-}
-
-function getPage($load, $page) {
-	global $load->view('header');
-	global $load->view($page);
-	global $load->view('footer');
-}*/
-
 ?>
