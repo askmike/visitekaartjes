b7906c980680f66c85b17b5fb5d8d8e8e81f6f20
Mike van Rossum <mike@askmike.org>
2011-12-01 15:12:18 +0100

    edited to work with the build script of boilerplate

--- views/footer.php
+++ views/footer.php
@@ -17,8 +17,10 @@
 		</div> <!--  right -->
 	</div>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+  <script>window.jQuery || document.write('<script src="static/js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+  <!-- scripts concatenated and minified via ant build script-->
   <script defer src="static/js/script.js"></script>
+  <!-- end scripts-->
 
   <!--[if lt IE 7 ]>
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
--- views/header.php
+++ views/header.php
@@ -7,13 +7,15 @@
 	<meta name=description content="visitekaartjes genator is een HvA schoolopdracht">
 	<meta name=author content="Mike van Rossum">
   	<meta name=viewport content="width=device-width,initial-scale=1">
+	<!-- CSS concatenated and minified via ant build script-->
 	<link rel=stylesheet href='static/css/style.css'>
+	<!-- end CSS-->
 </head>
 <body class='<?php echo $request; ?>'> 
   <div id="container">
     <header>
 		<h1 class='ir'>
 			Visitekaartjes Generator
-		</h1><?php echo $menu;?> <span class='code'>(<a href="https://github.com/askmike/visitekaartjes">check hier de code</a>)</span>
+		</h1><?php echo $menu;?> <span class='code'>(alles is minified: <a href="https://github.com/askmike/visitekaartjes">check hier de code</a>)</span>
     </header>
     <div id="main" role="main">
