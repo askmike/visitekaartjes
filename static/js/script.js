762283edcd7e1abf6c7482a2c96483d761e9bb3f
Mike van Rossum <mike@askmike.org>
2011-12-01 15:35:09 +0100

    improved js functions, no code is on more than 1 place now

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,22 @@ $(function(){
 	$('.result').each(animate());
 	//ajax
 	$('.ajax').each(function(){
+		processForm(function(){
+			$.getJSON(window.location.href + '/../json?naam=' + naam + '&beroep=' + beroep,
+			function(data) {
+				insertCards(data.naam, data.beroep);
+			});
+		});
+	});
+	//javascript
+	$('.js').each(function(){
+			processForm(function() {
+				insertCards(naam, beroep);
+			}); 
+
+	});
+	
+	function processForm(callback) {
 		$('form').submit(function(e) {
 			//stop the form from being sent
 			e.preventDefault();
@@ -11,43 +27,25 @@ $(function(){
 			naam = $('#naam').val();
 			beroep = $('#beroep').val();
 			//check if everything is filled
-			if (beroep && naam) {
-				//do a request and POST the vars
-				$.getJSON(window.location.href + '/../json?naam=' + naam + '&beroep=' + beroep,
-				function(data) {
-					for(i = 0; i < 10; i++) {
-						$('.kaartje.template').clone().removeClass('template').addClass('klaar').appendTo('#container');
-					}
-					$('.kaartje.klaar').find('.rotate').prepend(data.naam).find('span').append(data.beroep);
-					animate();
-				});
+			if(naam && beroep) {
+				callback();
 			}
 		});
-	});
-	//javascript
-	$('.js').each(function(){
-			$('form').submit(function(e) {
-				//stop the form from being sent
-				e.preventDefault();
-				//grab the input
-				naam = $('#naam').val();
-				beroep = $('#beroep').val();
-				//check if everything is filled
-				if (beroep && naam) {
-					for(i = 0; i < 10; i++) {
-						$('.kaartje.template').clone().removeClass('template').addClass('klaar').appendTo('#container');
-					}
-					$('.kaartje.klaar').find('.rotate').prepend(naam).find('span').append(beroep);
-					animate();
-				}
-			});
-	});
+	}
 	
 	function animate() {
 		count = $('.klaar').size() - 1;
 		$('.klaar').not('#template').each(function(i) {
 			$(this).delay((count-i)*500).slideDown(500);
 		});
+	}
+	
+	function insertCards(naam, beroep) {
+		for(i = 0; i < 10; i++) {
+			$('.kaartje.template').clone().removeClass('template').addClass('klaar').appendTo('#container');
+		}
+		$('.kaartje.klaar').find('.rotate').prepend(naam).find('span').append(beroep);
+		animate();
 	}
 });
 
